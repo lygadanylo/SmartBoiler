ca2527bf77c25871080c5445367ba2c3ce4813fa
Danylo Lyga <dlyg@keplercode.com>
2020-01-21 21:11:09 +0200

    init socket connection .apk <====> server

--- House-server/server.js
+++ House-server/server.js
@@ -1,32 +1,48 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const http = require('http');
-const WebSocketServer = require('websocket').server;
-const os = require('os');
+const express = require("express");
+const bodyParser = require("body-parser");
+const http = require("http");
+// const WebSocketServer = require("websocket").server;
+const webSocket = require("ws");
+const uuid = require("uuid");
+const os = require("os");
 
 const HOST = os.networkInterfaces().Ethernet[1].address;
-const APP_PORT = 3000;
-const WEB_PORT = 8080;
+const APP_PORT = 80;
+const WEB_PORT = 81;
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser());
 
-app.get('/', (req, res) => {
-	console.log('in');
-	return res.status(200).json({ temp: '32 dig' });
+app.get("/", (req, res) => {
+  const SID = uuid.v4();
+  return res.status(200).json({ SID: SID });
 });
 
 app.listen(APP_PORT, HOST, () => {
-	console.log(`Server working on PORT - ${APP_PORT}, HOST - ${HOST}`);
+  console.log(`Server listen on port - ${APP_PORT} and host - ${HOST}`);
 });
 
-// Web Socket initialization
-const server1 = http.createServer((request, response) => {});
+const server = http.createServer((requset, response) => {});
 
-server1.listen(WEB_PORT, () => {
-	console.log(`Web Socket server listening on WEB PORT - ${WEB_PORT}`);
+server.listen(WEB_PORT, () => {
+  console.log(`Socket server working on PORT - ${WEB_PORT}`);
 });
 
-const wsServer = new WebSocketServer({httpServer:server1});
+const wsServer = new webSocket.Server({ server });
+
+wsServer.on("connection", socket => {
+  socket.on("open", () => {
+    console.log("Connection opened");
+  });
+  socket.on("message", data => {
+    console.log(data);
+  });
+  socket.on("error", error => {
+    console.log(error);
+  });
+  socket.on("close", () => {
+    console.log("Connection closed");
+  });
+});
--- smartHouseBeta/components/actions.js
+++ smartHouseBeta/components/actions.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+let ws;
 
 export const tempData = data => ({
   type: 'TEMP_DATA',
@@ -8,10 +9,23 @@ export const tempData = data => ({
 export const FeatchData = () => dispatch => {
   axios({
     method: 'GET',
-    url: 'http://192.168.0.108:3000/',
+    url: 'http://192.168.0.108:80/',
   })
     .then(response => {
-      dispatch(tempData(response.data.temp));
+      dispatch(tempData(response.data.SID));
+      ws = new WebSocket('ws://192.168.0.108:81');
+      ws.onopen = () => {
+        console.log('Socket connection is open');
+      };
+      ws.onmessage = data => {
+        console.log(data);
+      };
+      ws.onerror = error => {
+        console.log(error);
+      };
+      ws.onclose = () => {
+        console.log('Socket connection is close');
+      };
     })
     .catch(error => {
       console.log(error);
