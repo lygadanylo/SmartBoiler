1d7627cedeffaff6f8e4c46b94f006a73a1a472e
Danylo Lyha <danylo.lyha@synapse.com>
2020-01-21 09:46:52 +0200

    init socket

--- House-server/server.js
+++ House-server/server.js
@@ -1,17 +1,32 @@
-const server = require("express")();
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+const os = require('os');
 
-const PORT = 3000;
-const HOST = "192.168.0.108";
+const HOST = os.networkInterfaces().Ethernet[1].address;
+const APP_PORT = 3000;
+const WEB_PORT = 8080;
 
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(bodyParser());
+const app = express();
 
-server.get("/", (req, res) => {
-  console.log("in");
-  return res.status(200).json({ temp: "32 dig" });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser());
+
+app.get('/', (req, res) => {
+	console.log('in');
+	return res.status(200).json({ temp: '32 dig' });
 });
 
-server.listen(PORT, HOST, () => {
-  console.log(`Server working on PORT - ${PORT}, HOST - ${HOST}`);
+app.listen(APP_PORT, HOST, () => {
+	console.log(`Server working on PORT - ${APP_PORT}, HOST - ${HOST}`);
 });
+
+// Web Socket initialization
+const server1 = http.createServer((request, response) => {});
+
+server1.listen(WEB_PORT, () => {
+	console.log(`Web Socket server listening on WEB PORT - ${WEB_PORT}`);
+});
+
+const wsServer = new WebSocketServer({httpServer:server1});
